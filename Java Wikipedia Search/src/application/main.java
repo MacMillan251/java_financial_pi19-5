55415f790487b1a324a0424d0346e8d79404b8f9
MacMillan251 <71598830+MacMillan251@users.noreply.github.com>
2020-12-22 02:28:41 +0300

    Add files via upload

--- Java Wikipedia Search/src/application/main.java
+++ Java Wikipedia Search/src/application/main.java
@@ -10,17 +10,20 @@ public class main {
 
         while (!repeat) {
 
+            //Пункт №1
             System.out.println("\nВведите ваш запрос >>");
             String nextLine = scanner.nextLine();
             String searchText = nextLine;
             if (nextLine == null)
                 searchText = "Wikipedia API";
             System.out.println("Поиск...");
+            //Пункт №2
             String wikipediaApi = "https://www.wikipedia.org/w/api.php?format=json&action=opensearch&prop=extracts&exintro=&explaintext=&search="
                     + searchText.replace(" ", "%20");
 
             UrlConnect apilink = new UrlConnect(wikipediaApi);
             String response = apilink.apireturn();
+            //Пункт №3,4
             response = response.replace("[", "");
             response = response.replace("]", "");
 
@@ -29,23 +32,24 @@ public class main {
                 if (element == '"') countSearch++;
             }
             countSearch = (countSearch / 2 - 1) / 3;
-
             System.out.println("\nКол-во найденных статей = " + countSearch + ":\n");
+            String output;
             for (int i = 1; i <= countSearch; i++) {
-                System.out.println((response.split("\",\"")[i] + " — " + response.split("\",\"")[i + countSearch * 2]));
+                output = (response.split("\",\"")[i] + " — " + response.split("\",\"")[i + countSearch * 2]);
+                System.out.println(output.replace("\"", ""));
             }
+            //Пункт №5
             if (countSearch == 1) {
                 searchText = response.split(",")[1];
                 searchText = searchText.replace("\"", "");
                 wikipediaApi = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
                         + searchText.replace(" ", "_");
-                //System.out.println(wikipediaApi);
                 UrlConnect apilink2 = new UrlConnect(wikipediaApi);
                 String description = apilink2.apireturn();
-                String desctext = description.split("extract\":\"")[1];
-                desctext = desctext.length() > 400 ? desctext.substring(0, 100) + "\n" + desctext.substring(100, 200)
-                        + "\n" + desctext.substring(200, 300) + "\n" +desctext.substring(300, 400): desctext;
-                System.out.println(desctext + "...");
+                output = description.split("extract\":\"")[1];
+                output = output.length() > 400 ? output.substring(0, 100) + "\n" + output.substring(100, 200)
+                        + "\n" + output.substring(200, 300) + "\n" + output.substring(300, 400) : output;
+                System.out.println(output + "...");
             }
         }
     }
