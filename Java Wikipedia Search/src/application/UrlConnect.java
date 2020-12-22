7dac74bdd01d5af266e6a316e69ae6abc76da0cf
MacMillan251 <71598830+MacMillan251@users.noreply.github.com>
2020-12-22 05:16:12 +0300

    Add files via upload

--- Java Wikipedia Search/src/application/UrlConnect.java
+++ Java Wikipedia Search/src/application/UrlConnect.java
@@ -4,26 +4,25 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
-public class UrlConnect{
+public class UrlConnect {
 
     private String link;
 
-    public UrlConnect(String link){
+    public UrlConnect(String link) {
 
-        this.link=link;
+        this.link = link;
     }
 
     public String apireturn() {
-        String response = "";
+        String response;
         try {
             HttpURLConnection wikihttp = (HttpURLConnection) new URL(link).openConnection();
             wikihttp.addRequestProperty("User-Agent", "Mozilla/5.0");
             BufferedReader in = new BufferedReader(new InputStreamReader(wikihttp.getInputStream()));
             response = in.lines().collect(Collectors.joining());
             in.close();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception ex) {
+            response = "error";
         }
         return response;
     }
--- Java Wikipedia Search/src/application/main.java
+++ Java Wikipedia Search/src/application/main.java
@@ -15,7 +15,7 @@ public class main {
             String nextLine = scanner.nextLine();
             String searchText = nextLine;
             if (nextLine == null)
-                searchText = "Wikipedia API";
+                continue;
             System.out.println("Поиск...");
             //Пункт №2
             String wikipediaApi = "https://www.wikipedia.org/w/api.php?format=json&action=opensearch&prop=extracts&exintro=&explaintext=&search="
@@ -26,30 +26,49 @@ public class main {
             //Пункт №3,4
             response = response.replace("[", "");
             response = response.replace("]", "");
-
-            int countSearch = 0;
-            for (char element : response.toCharArray()) {
-                if (element == '"') countSearch++;
-            }
-            countSearch = (countSearch / 2 - 1) / 3;
-            System.out.println("\nКол-во найденных статей = " + countSearch + ":\n");
+            int countRes = 0;
             String output;
-            for (int i = 1; i <= countSearch; i++) {
-                output = (response.split("\",\"")[i] + " — " + response.split("\",\"")[i + countSearch * 2]);
-                System.out.println(output.replace("\"", ""));
+            if (response != "error") {
+                try {
+                    for (char element : response.toCharArray()) {
+                        if (element == '"') countRes++;
+                    }
+                    countRes = (countRes / 2 - 1) / 3;
+                    System.out.println("\nКол-во найденных статей = " + countRes + ":\n");
+
+                    for (int i = 1; i <= countRes; i++) {
+                        output = (response.split("\",\"")[i] + " — " + response.split("\",\"")[i + countRes * 2]);
+                        System.out.println(output.replace("\"", ""));
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Некорректный ввод запроса");
+                }
+            } else {
+                System.out.println("Некорректный ввод запроса");
             }
+
             //Пункт №5
-            if (countSearch == 1) {
-                searchText = response.split(",")[1];
-                searchText = searchText.replace("\"", "");
-                wikipediaApi = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
-                        + searchText.replace(" ", "_");
-                UrlConnect apilink2 = new UrlConnect(wikipediaApi);
-                String description = apilink2.apireturn();
-                output = description.split("extract\":\"")[1];
-                output = output.length() > 400 ? output.substring(0, 100) + "\n" + output.substring(100, 200)
-                        + "\n" + output.substring(200, 300) + "\n" + output.substring(300, 400) : output;
-                System.out.println(output + "...");
+            if (countRes == 1) {
+                try {
+
+                    searchText = response.split(",")[3];
+                    searchText = searchText.replace("\"", "");
+                    wikipediaApi = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
+                            + searchText.replace("https://en.wikipedia.org/wiki/", "");
+                    UrlConnect apilink2 = new UrlConnect(wikipediaApi);
+                    String description = apilink2.apireturn();
+                    output = description.split("extract\":\"")[1];
+                    output = output.length() > 400 ? output.substring(0, 100) + "\n" + output.substring(100, 200)
+                            + "\n" + output.substring(200, 300) + "\n" + output.substring(300, 400) : output;
+                    if (output.length() > 20) {
+                        System.out.println(output.replace("\"}}}}", "") + "...");
+                    } else {
+                        System.out.println("Описание недоступно");
+                    }
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Описание недоступно");
+                }
             }
         }
     }
